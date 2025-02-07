458841cf77df4f9e097d9197a45804f0df0f83ed
Derek Fujimoto <dfujimoto@triumf.ca>
2025-02-07 13:14:08 -0800

    Fixed small typos

--- ShimController.py
+++ ShimController.py
@@ -17,6 +17,7 @@ class ShimController(object):
 
     Args:
         device (str): connect to a device at this location
+        zeroed (bool): if true, start and don't set setpoints, otherwise set according to last set values'
         debug (bool): if true, print debugging statements
 
     Attributes:
@@ -24,6 +25,12 @@ class ShimController(object):
         calib (pd.DataFrame): calibration constants and channel mapping
         debug (bool): if true print debug statements
         setpoints (pd.DataFrame): set currents and voltages
+
+    Notes:
+        this object writes to the file self.FILE_SETPOINTS every time a value is
+        sent to the arduino. This ensures a record of the last set of values. It
+        also allows the object to restore the last set of points. We can also save
+        these values to a user-defined file and load that.
     """
 
     # file for calibration constants
@@ -35,24 +42,41 @@ class ShimController(object):
     # number of shim coils
     NLOOPS = 64
 
-    def __init__(self, device, debug=False):
+    def __init__(self, device, zeroed=True, debug=False):
 
         self.debug = debug
 
         # get calibration file
         self.calib = pd.read_csv(self.FILE_CALIBRATION, comment='#', index_col=0)
 
+        # setup current setpoints dataframe
+        self.read_setpoints(setall=False)
+
         # connect to device
         self.arduino = ArduinoControllerCS(device, quiet=not debug)
 
-        # setup current settings dataframe
-        self.setpoints = pd.DataFrame()
+        # rezero
+        if zeroed:
+            self.zero_voltage()
+
+        # write values to arduino
+        self.set_all_setpoints()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.disconnect()
 
     def _update_setpoints(self, coil, current, voltage):
         # update the dataframe with new values
         self.setpoints.loc[coil, 'current'] = current
         self.setpoints.loc[coil, 'voltage'] = voltage
         self.write_setpoints()
+
+    def disconnect(self):
+        """Close the serial connection to the arduino"""
+        self.arduino.disconnect()
 
     def set_all_setpoints(self):
         """Set all currents to their respective setpoints"""
@@ -116,11 +140,17 @@ class ShimController(object):
         current = (volts-cal.offset)/cal.slope
         self._update_setpoints(coil, voltage=volts, current=current)
 
-    def read_setpoints(self, filename=None):
-        """Read setpoints file so as to load the last values set. Expect columns "coil", "voltage", and "current" where only one of voltage or current is needed. "coil" must be the leftmost column.
+    def read_setpoints(self, filename=None, setall=False):
+        """Read setpoints file so as to load the last values set, write this to the arduino.
+
+        Notes:
+            Expect columns "coil", "voltage", and "current"
+            Only one of "voltage" or "current" is needed.
+            Column "coil" must be the leftmost column.
 
         Args:
             filename (str): file path, if none use default self.FILE_SETPOINTS
+            setall (bool): if true, write the values to the arduino
         """
 
         # default filename
@@ -143,6 +173,9 @@ class ShimController(object):
         # set setpoints
         self.setpoints = setpts
 
+        if setall:
+            self.set_all_setpoints()
+
     def write_setpoints(self, filename=None):
         """Write setpoints file so as to save the last values set
 
@@ -159,18 +192,17 @@ class ShimController(object):
             lines = []
             for line in fid:
                 if line.startswith('#') or line.strip() == '':
-                    lines.append(line)
+                    lines.append(line.strip())
                 else:
                     break
 
         # update the datetime
-        lines[-1] = str(datetime.now())
+        lines[-1] = f'# {datetime.now()}'
 
         # write the new file
         with open(filename, 'w') as fid:
             fid.write('\n'.join(lines))
             fid.write('\n')
-
         self.setpoints.to_csv(filename, mode='a')
 
     def zero_current(self):
@@ -186,3 +218,4 @@ class ShimController(object):
         # update all setpoints
         self.setpoints.voltage = 0
         self.setpoints.current = -self.calib.offset/self.calib.slope
+        self.write_setpoints()
